52d0828808ba29a50c3cb80a716770030bc7001c
Sarvesh0007 <sarveshmm99@gmail.com>
2021-06-20 20:02:25 +0530

    chnaged name to jabber

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ io.on('connection', socket => {
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit('message', formatMessage(botName, 'Welcome to LetsChat'));
+    socket.emit('message', formatMessage(botName, 'Welcome to jabber'));
 
     // Broadcast when a user connects
     socket.broadcast
